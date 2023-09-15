2b1fef5227f20417cbe5bb471177f9f01715cb2a
Stepanxan <stepanxan@gmail.com>
2023-09-15 16:35:47 +0300

    verify2

--- scraping.py
+++ scraping.py
@@ -6,7 +6,7 @@ import json
 import threading
 from queue import Queue
 
-# Скрапінг сайту для отримання останніх 10 транзакцій та витягування з них гаманця(From) який записується в scraped_data.json
+# Скрапінг сайту для отримання останніх 5-ти транзакцій та витягування з них гаманця(From) який записується в scraped_data.json
 def scrape_transaction_data(url):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -47,7 +47,7 @@ if response.status_code == 200:
         if data:
             scraped_data[idx + 1] = data
 
-    end_scraping_time = time.time()  # Завершення вимірювання часу скрапінгу
+    end_scraping_time = time.time()
     print(f"Scraping took {end_scraping_time - start_scraping_time} seconds")
     with open("scraped_data.json", "w", encoding="utf-8") as json_file:
         json.dump(scraped_data, json_file, ensure_ascii=False, indent=4)
